22836843854a5169e28bd044c8f66609ac7cc0fe
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2025-11-20 11:06:38 +0800

    Tags

--- src/main/java/com/besson/tutorial/datagen/ModBlockTagsProvider.java
+++ src/main/java/com/besson/tutorial/datagen/ModBlockTagsProvider.java
@@ -2,6 +2,7 @@ package com.besson.tutorial.datagen;
 
 import com.besson.tutorial.TutorialMod;
 import com.besson.tutorial.block.ModBlocks;
+import com.besson.tutorial.tag.ModBlockTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.tags.BlockTags;
@@ -24,5 +25,16 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
 
         this.tag(BlockTags.NEEDS_IRON_TOOL)
                 .add(ModBlocks.ICE_ETHER_ORE.get());
+
+        this.tag(ModBlockTags.ORE_TAGS)
+                .add(ModBlocks.ICE_ETHER_ORE.get())
+                .addTag(BlockTags.COAL_ORES)
+                .addTag(BlockTags.IRON_ORES)
+                .addTag(BlockTags.GOLD_ORES)
+                .addTag(BlockTags.DIAMOND_ORES)
+                .addTag(BlockTags.EMERALD_ORES)
+                .addTag(BlockTags.LAPIS_ORES)
+                .addTag(BlockTags.REDSTONE_ORES);
+
     }
 }
